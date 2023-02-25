55e513ea3de5eaf9c44547d4decd507085dcf1d9
ftmtrm <fatostarim@gmail.com>
2023-02-25 18:51:04 +0300

    v3

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -1,8 +1,5 @@
 public class Deneme {
     public static void main(String[] args) {
         System.out.println("Merhaba");
-        //restore tamamen working space'i temizler
-        //stash silmeden hafizaya alir geri getirebiliriz.gecici olarak kaldirir.
-
     }
 }
